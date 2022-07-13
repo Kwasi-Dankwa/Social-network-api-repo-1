f882265b5ffea9f308c6bfa5d6d7dc4a0a532791
Kwasi Dankwa <edankwa14@gmail.com>
2022-07-12 22:26:33 -0400

    updating server file and fixing naming errors

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose').MongoClient;
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.json());
+app.use(routes);
+
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server is running on port: ${PORT}!`);
+  });
+});
+  
 
