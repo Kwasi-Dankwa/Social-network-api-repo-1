bbce6b3f166a58681a3ac06848a0009357d60930
Kwasi Dankwa <edankwa14@gmail.com>
2022-07-13 19:54:58 -0400

    fixing connectio to db and schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,39 +1,6 @@
 const { Schema, model, Types } = require('mongoose');
 const moment = require("moment");
 
-//thought schema
-const thoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            minLength: 1,
-            maxLength: 280,
-        },
-
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
-        },
-
-        username: {
-            type: String,
-            required: true,
-        },
-
-        reactions: [reactionSchema],
-
-    },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true,
-        },
-        id: false,
-    }
-);
-
 //reaction schema
 const reactionSchema = new Schema (
     {
@@ -68,6 +35,38 @@ const reactionSchema = new Schema (
     }
 )
 
+//thought schema
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+        reactions: [reactionSchema],
+
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
 
 
 // Initialize the Thought model using userSchema
